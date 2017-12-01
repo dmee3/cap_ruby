999a2584049fab4f9a24533eddfb94d04a0acca2
Dan <dmeehan@covermymeds.com>
2017-12-01 18:34:53 -0500

    Let bot repeat scores

--- bot/points.rb
+++ bot/points.rb
@@ -1,27 +1,51 @@
 class Points < SlackRubyBot::Commands::Base
-  match /^([\s\w'@.\-:]*)\s*([-+]{2}|—)(?:\s+(?:for|because|cause|cuz)\s+(.+))?$/i do |client, data, matches|
+  match /^lil_botty([\s\w'@.\-:<>@]*)\s*([-+]{2}|—)(?:\s+(?:for|because|cause|cuz)\s+(.+))?$/i do |client, data, matches|
     name = matches[1]&.strip
     op = matches[2]&.strip
     reason = matches[3]&.strip
 
-    if !name.nil?
-      subject = BotPoint.find_or_create_by(name: name, room: data.channel)
-      entry = BotPointEntry.where(bot_point_id: subject.id)
-                            .where(reason: reason)
-                            .first_or_create
+    if name.blank?
+      subject, entry = find_last_score(data.channel)
+      name = subject.name
+    else
+      subject, entry = find_or_create_score(name, data.channel, reason)
+    end
 
+    update_score(op, entry)
+
+    points = BotPointEntry.where(bot_point_id: subject.id).sum(:score)
+
+    response = "#{name} has #{points} point#{points.abs == 1 ? '' : 's'}"
+    unless reason.nil?
+      response << ", #{entry.score} of which #{entry.score.abs == 1 ? 'is' : 'are'} for #{entry.reason}"
+    end
+
+    client.say(channel: data.channel, text: response)
+  end
+
+  class << self
+    def update_score(op, entry)
       entry.score = 0 if entry.score.nil?
       entry.score += (op == '++' ? 1 : -1)
       entry.save
+    end
 
-      points = BotPointEntry.where(bot_point_id: subject.id).sum(:score)
+    def find_last_score(room)
+      subject = BotPoint.where(room: room)
+                        .order('updated_at DESC')
+                        .first
+      entry = BotPointEntry.where(bot_point_id: subject.id)
+                           .order('updated_at DESC')
+                           .first
+      [subject, entry]
+    end
 
-      response = "#{name} has #{points} point#{points.abs == 1 ? '' : 's'}"
-      unless reason.nil?
-        response << ", #{entry.score} of which #{entry.score.abs == 1 ? 'is' : 'are'} for #{entry.reason}"
-      end
-
-      client.say(channel: data.channel, text: response)
+    def find_or_create_score(name, room, reason)
+      subject = BotPoint.find_or_create_by(name: name, room: room)
+      entry = BotPointEntry.where(bot_point_id: subject.id)
+                           .where(reason: reason)
+                           .first_or_create
+      [subject, entry]
     end
   end
 end
