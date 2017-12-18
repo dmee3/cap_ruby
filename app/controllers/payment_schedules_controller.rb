abd72a047afec2852c6ce168659470519a72a3f6
Dan <dmeehan@covermymeds.com>
2017-12-17 19:48:28 -0500

    Order payment schedule entries

--- app/controllers/payment_schedules_controller.rb
+++ app/controllers/payment_schedules_controller.rb
@@ -3,7 +3,7 @@ class PaymentSchedulesController < ApplicationController
   before_action -> { redirect_if_not 'admin' }
 
   def show
-    @schedule = PaymentSchedule.includes(:payment_schedule_entries).find params[:id]
+    @schedule = PaymentSchedule.includes(:payment_schedule_entries).find(params[:id]).order(:pay_date)
     @user = User.find @schedule.user_id
     respond_to do |format|
       format.json { render json: @schedule, include: [:payment_schedule_entries] }
