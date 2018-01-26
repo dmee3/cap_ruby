d268adca226db5224a24d08c602eb0e62eadd761
Dan <dmeehan@covermymeds.com>
2018-01-26 14:18:56 -0500

    Lower-case reasons

--- bot/place.rb
+++ bot/place.rb
@@ -1,22 +1,22 @@
 class Place < SlackRubyBot::Commands::Base
   PLACES = *(5..25).freeze # Array from Range using splat operator
   BAD_REASONS = [
-    'Somebody forgot the flashlights',
+    'somebody forgot the flashlights',
     'Dan left the kingpin key in his car',
-    "The uniforms weren't in yet",
+    "the uniforms weren't in yet",
     "Nola is on the panel... _we're done here!_",
-    "The snares keep missing for corps camps and can't play together",
-    "We couldn't connect to the router in the mixer cart",
+    "the snares keep missing for corps camps and can't play together",
+    "we couldn't connect to the router in the mixer cart",
     "Stew couldn't stop puking in the lot",
     'Shaun got lost picking up the trailer',
-    'The bass solo changed again',
-    'They mispronounced our show name.  Wentu Worlds, Co. Lied',
-    "We were just bad",
-    'We left part of the prop in the lot',
-    "The generator wouldn't start for warmup",
-    'Fucking STRYKE and that hot redhead, man',
-    'We took a huge timing penalty',
-    'The staff unfolded the tarp wrong'
+    'the bass solo changed again',
+    'they mispronounced our show name.  Wentu Worlds, Co. Lied',
+    "we were just bad",
+    'we left part of the prop in the lot',
+    "the generator wouldn't start for warmup",
+    'fucking STRYKE and that hot redhead, man',
+    'we took a huge timing penalty',
+    'the staff unfolded the tarp wrong'
   ].freeze
   GOOD_REASONS = [
     'Things actually went pretty well for once',
