759de4c797cfc70d1bb88a2c4b2af8bdd9753755
Dan <dmeehan@covermymeds.com>
2018-10-22 19:28:51 -0400

    Starting on specs

--- spec/models/conflict_spec.rb
+++ spec/models/conflict_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Conflict, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    subject { Conflict.new }
+
+    it 'requires an end_date' do
+      expect(subject).not_to be_valid
+    end
+  end
 end
