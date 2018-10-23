a820e768f380db611fd3824d852b5d40ed088909
Dan <dmeehan@covermymeds.com>
2018-10-22 23:05:43 -0400

    Add some model specs

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    subject { build(:payment) }
+
+    it { is_expected.to be_valid }
+
+    it 'requires an amount' do
+      subject.amount = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'requires a date paid' do
+      subject.date_paid = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'requires a payment type' do
+      subject.payment_type_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'requires a season' do
+      subject.season_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'requires a user' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
