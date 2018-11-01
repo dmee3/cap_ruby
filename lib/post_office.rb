d30135889b7e799746ae6883b9a3236659f1363b
Dan <dmeehan@covermymeds.com>
2018-11-01 14:52:32 -0400

    moar testing

--- lib/post_office.rb
+++ lib/post_office.rb
@@ -1,8 +1,8 @@
 class PostOffice
   class << self
     def send_email(to, subject, text)
-      return unless Rails.env.production?
       client.send_message(
+        'mg.capcitypercussion.com',
         from: 'donotreply@mg.capcitypercussion.com',
         to: to,
         subject: subject,
@@ -14,6 +14,12 @@ class PostOffice
 
     def client
       @client ||= Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+      if Rails.env.production?
+        @client.disable_test_mode!
+      else
+        @client.enable_test_mode!
+      end
+      @client
     end
   end
 end
