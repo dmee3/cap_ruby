564a593e3d08c66209b790165a5e2b01c1fddda4
Dan <dmeehan@covermymeds.com>
2019-03-11 00:29:37 -0400

    Trying another command

--- bot/recent_payments.rb
+++ bot/recent_payments.rb
@@ -5,28 +5,47 @@ class RecentPayments < SlackRubyBot::Commands::Base
                       .order(date_paid: :desc, created_at: :desc)
                       .limit(limit)
 
-    response = "The #{payments.length} most recent payments are:"
+    header_text = "The #{payments.length} most recent payments are:"
+    client.say(
+      channel: data.channel,
+      text: header_text << generate_response(payments)
+    )
+  end
 
-    payment_metrics = { total: 0, types: {} }
+  match(%r{^(?:<@U885J7NDS>\s*)?who paid since (\d+)/(\d+)$}i) do |client, data, match|
+    limit_date = "#{match[1]}/#{match[2]}"
 
-    payments.each do |p|
-      amount = p.amount.to_f / 100.0
-      payment_type = p.payment_type.name
+    payments = Payment.includes(:user, :payment_type)
+                      .where('date_paid >= ?', "#{limit_date}/2019")
+                      .order(date_paid: :desc, created_at: :desc)
 
-      payment_metrics[:total] += amount
-      payment_metrics[:types][payment_type] = (payment_metrics[:types][payment_type] || 0) + amount
-
-      response << "\n*#{p.user.full_name}* _#{format('$%.2f', amount)}_"
-      response << "\n>#{p.date_paid.strftime('%a, %-m/%-d')} - #{payment_type}"
-    end
-
-    response << "\n\nTotals:"
-    response << generate_metrics(payment_metrics)
-
-    client.say(channel: data.channel, text: response)
+    header_text = "The payments since #{limit_date} are:"
+    client.say(
+      channel: data.channel,
+      text: header_text << generate_response(payments)
+    )
   end
 
   class << self
+    def generate_response(payments)
+      ''.tap do |res|
+        payment_metrics = { total: 0, types: Hash.new(0) }
+        payments.each do |p|
+          amount = p.amount.to_f / 100.0
+          type = p.payment_type.name
+
+          payment_metrics[:total] += amount
+          payment_metrics[:types][type] += amount
+
+          res << "\n*#{p.user.full_name}* _#{format('$%.2f', amount)}_"
+          res << "\n>#{p.date_paid.strftime('%a, %-m/%-d')} - #{type}"
+        end
+
+        res << "\n\nTotals:"
+        res << generate_metrics(payment_metrics)
+      end
+    end
+
     def generate_metrics(metrics)
       "\n".tap do |res|
         metrics[:types].each { |type, amt| res << "\n> #{format('$%.2f', amt)} _#{type}_" }
