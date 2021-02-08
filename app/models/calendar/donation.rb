3581672cb884734c33a568cffe18d37b5806c392
Dan Meehan <dan.meehan17@gmail.com>
2021-02-08 11:10:27 -0500

    annotate calendar model

--- app/models/calendar/donation.rb
+++ app/models/calendar/donation.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: calendar_donations
+#
+#  id            :integer          not null, primary key
+#  amount        :integer
+#  donation_date :integer
+#  donor_name    :string
+#  notes         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#
+# Indexes
+#
+#  index_calendar_donations_on_user_id  (user_id)
+#
 class Calendar::Donation < ApplicationRecord
   belongs_to :user
   validates :amount, presence: true
