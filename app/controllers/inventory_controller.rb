66ef528d2820f73a53fe48e8a17688affd90fb41
Dan Meehan <dan.meehan17@gmail.com>
2021-10-27 12:46:01 -0400

    Dynamically set inventory layout

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -2,8 +2,6 @@ class InventoryController < ApplicationController
   before_action :authenticate_user!
   before_action :redirect_if_no_inventory_access
 
-  layout 'inventory'
-
   def redirect_if_no_inventory_access
     redirect_to(root_url) unless current_user_role == 'admin' || current_user.quartermaster?
   end
