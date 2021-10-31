4c7899a882adfb3ad2df1b1f0e94f86a1a261761
Dan Meehan <dan.meehan17@gmail.com>
2021-10-31 11:46:44 -0400

    Fix member dashboard payment schedule entry ordering

--- app/controllers/members/dashboard_controller.rb
+++ app/controllers/members/dashboard_controller.rb
@@ -7,10 +7,11 @@ module Members
 
       @payments = current_user.payments_for(current_season['id']).sort_by(&:date_paid)
       @payment_schedule = current_user.payment_schedule_for(current_season['id'])
-      @next_payment_date = @payment_schedule.entries.select { |e| e.pay_date >= Date.today }.first || @payment_schedule.entries.last
+      @entries = @payment_schedule.entries.order(:pay_date)
+      @next_payment_date = @entries.select { |e| e.pay_date >= Date.today }.first || @entries.last
       @next_payment_amount = PaymentService.amount_owed_on_date(current_user, @next_payment_date.pay_date, current_season['id'])
       @paid = @payments.sum(&:amount) / 100
-      @total_dues = @payment_schedule.entries.sum(:amount) / 100
+      @total_dues = @entries.sum(:amount) / 100
 
       @next_event = EventService.next_event
     end
