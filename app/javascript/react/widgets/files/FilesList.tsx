fc5374e0340d9f7c845585e5fba7a958145d9dd4
Dan Meehan <dan.meehan17@gmail.com>
2021-11-06 21:32:45 -0400

    Finish member side dark mode

--- app/javascript/react/widgets/files/FilesList.tsx
+++ app/javascript/react/widgets/files/FilesList.tsx
@@ -48,22 +48,21 @@ const FilesList = ({
       })
   }, [expanded])
 
+  const loadingElement = (
+      <li className="pl-9 py-4 flex flex-col">
+      <div className="flex justify-start">
+        <div className="rounded-full bg-gray-300 dark:bg-gray-600 h-6 w-6 animate-pulse"></div>
+        <div className="ml-2 w-48 bg-gray-300 dark:bg-gray-600 rounded animate-pulse"></div>
+      </div>
+    </li>
+  )
+
   return (
     <>
       {loading &&
         <ul className="divide-y divide-gray-300">
-          <li className="pl-9 py-4 flex flex-col">
-            <div className="flex justify-start">
-              <div className="rounded-full bg-gray-300 h-6 w-6 animate-pulse"></div>
-              <div className="ml-2 w-48 bg-gray-300 rounded animate-pulse"></div>
-            </div>
-          </li>
-          <li className="pl-9 py-4 flex flex-col">
-            <div className="flex justify-start">
-              <div className="rounded-full bg-gray-300 h-6 w-6 animate-pulse"></div>
-              <div className="ml-2 w-48 bg-gray-300 rounded animate-pulse"></div>
-            </div>
-          </li>
+          {loadingElement}
+          {loadingElement}
         </ul>
       }
       {files.length === 0 && !loading &&
