ae51814e45a9b341d0cd9865a14137794d1dc815
Dan Meehan <dan.meehan17@gmail.com>
2021-11-09 13:36:21 -0500

    Correct welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
 
-    @url = new_user_password_path
+    @url = new_user_password_url
     mail(to: @user.email, subject: 'Welcome to Cap City')
   end
 end
