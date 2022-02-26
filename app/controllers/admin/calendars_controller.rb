eba00898da0637432f33b27c682658e6c79c899d
Dan Meehan <dan.meehan17@gmail.com>
2022-02-26 16:11:35 -0500

    Admin calendar view updates

--- app/controllers/admin/calendars_controller.rb
+++ app/controllers/admin/calendars_controller.rb
@@ -5,12 +5,15 @@ module Admin
     def index
       @total = donations.sum(&:donation_date)
       @donations_by_person = donations.group_by { |d| d.user.full_name }
+      User.members_for_season(Season.last.id).map(&:full_name).each do |u|
+        @donations_by_person[u] ||= []
+      end
     end
 
     private
 
     def donations
-      @donations ||= Calendar::Donation.all
+      @donations ||= Calendar::Donation.where(season_id: Season.last.id)
     end
   end
 end
