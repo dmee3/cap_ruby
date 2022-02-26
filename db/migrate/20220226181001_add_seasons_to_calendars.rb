9405e929e1e6d8d94dda412cecf924ed64b93a1f
Dan Meehan <dan.meehan17@gmail.com>
2022-02-26 13:47:10 -0500

    Add season to calendars

---
+++ db/migrate/20220226181001_add_seasons_to_calendars.rb
@@ -0,0 +1,6 @@
+class AddSeasonsToCalendars < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :calendar_donations, :season, index: true
+    add_foreign_key :calendar_donations, :seasons
+  end
+end
