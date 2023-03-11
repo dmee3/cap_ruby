9b51fac9e49e3f284fb091f8308769164352e2ab
Dan Meehan <dan.meehan17@gmail.com>
2023-03-11 15:14:23 -0500

    Add more calendars

--- app/javascript/react/widgets/members/CalendarChooser.tsx
+++ app/javascript/react/widgets/members/CalendarChooser.tsx
@@ -11,7 +11,9 @@ const CalendarChooser = ({
     'calendar_1', 'calendar_2', 'calendar_3', 'calendar_4', 'calendar_5', 'calendar_6',
     'calendar_7', 'calendar_8', 'calendar_9', 'calendar_10', 'calendar_11', 'calendar_12',
     'calendar_13', 'calendar_14', 'calendar_15', 'calendar_16', 'calendar_17', 'calendar_18',
-    'calendar_19', 'calendar_20',
+    'calendar_19', 'calendar_20', 'calendar_21', 'calendar_22', 'calendar_23', 'calendar_24',
+    'calendar_25', 'calendar_26', 'calendar_27', 'calendar_28', 'calendar_29', 'calendar_30',
+    'calendar_31', 'calendar_32',
   ]
 
   const pickImg = (img) => {
