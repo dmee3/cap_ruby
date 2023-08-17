fbd801ed01eb86fed193ff8e13de666c2f3fa53c
Dan Meehan <dan.meehan17@gmail.com>
2023-08-17 00:32:49 -0400

    Update registration sheet

--- app/services/auditions/registration.rb
+++ app/services/auditions/registration.rb
@@ -19,29 +19,37 @@ module Auditions
       'Email' => :email,
       'City' => :city,
       'State' => :state,
+      'Preferred Pronouns' => :pronouns,
+      'Shoe Size' => :shoe_size,
+      'Shirt Size' => :shirt_size,
       'Primary Instrument' => :instrument,
       'Birthdate' => :birthdate,
-      'Experience' => :experience
+      'Experience' => :experience,
+      'Known Conflicts' => :conflicts
     }.freeze
 
     class << self
       def header_row
-        ['Name', 'Email', 'City', 'State', 'Birthdate', 'Downloaded', 'Experience']
+        ['Name', 'Email', 'City', 'State', 'Pronouns', 'Shoe', 'Shirt', 'Birthdate', 'Purchased', 'Experience', 'Conflicts']
       end
     end
 
     # rubocop:disable Metrics/AbcSize
     def initialize(args)
       @type = TYPE_MAP[args[:type]]
+      @date = args[:date] - 4.hours
+
       @name = args[:name]
       @email = args[:email]
-
       @city = args[:city]&.titleize
       @state = args[:state] ? StateConverterService.abbreviation(args[:state]) : ''
+      @pronouns = args[:pronouns]
+      @shoe_size = args[:shoe_size]
+      @shirt_size = args[:shirt_size]
       @instrument = args[:instrument]
-      @date = args[:date] - 4.hours
-      @experience = args[:experience]
       @birthdate = args[:birthdate]
+      @experience = args[:experience]
+      @conflicts = args[:conflicts]
     end
     # rubocop:enable Metrics/AbcSize
 
@@ -51,9 +59,13 @@ module Auditions
         @email,
         @city,
         @state,
+        @pronouns,
+        @shoe_size,
+        @shirt_size,
         @birthdate,
         @date.strftime('%-m/%-d %-l:%M %P'),
-        @experience
+        @experience,
+        @conflicts
       ]
     end
   end
